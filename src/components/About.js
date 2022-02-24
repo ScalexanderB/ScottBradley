5775b8276a1a545d8488f2d79784e831e3e25f56
Scott Bradley <scottabradley91@gmail.com>
2022-02-24 17:44:05 -0500

    added swiper package for portfolio slides and refined about page

--- src/components/About.js
+++ src/components/About.js
@@ -2,16 +2,20 @@ import React from "react";
 
 function About() {
     return (
-        <div>
-            <h3 className="content is-medium">Hi, I'm Scott Bradley.</h3>
+        <div class="about-box">
             <hr />
-            <img className="my-pic" src={process.env.PUBLIC_URL + '/img/LinkedIn picture.jpg'} alt="Scott Bradley"/>
-            <p className="content is-italic mt-4">
+            <img src={process.env.PUBLIC_URL + '/img/LinkedIn picture.jpg'} alt="Scott Bradley"/>
+            <div>
+            <h2 className="content is-medium">Hi, I'm Scott Bradley.</h2>
+            <p className="content mt-4 textBox">
             I'm an aspiring web developer with a knack for problem solving and a passion for technology.
-            </p>
-            <p className="content">
+            
+            <br></br>
+            <br></br>
             I'm currently studying Web Development at UofT SCS, and I strive to create accessible digital products and scalable solutions.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae odio excepturi nemo. Vel laborum minima, debitis possimus facere quam iste tempora magni temporibus suscipit repellendus exercitationem non sit unde quisquam maxime ratione quibusdam, distinctio corrupti itaque rem quo amet earum necessitatibus? Aliquam, animi? Fuga cupiditate aperiam inventore. Nobis similique, minima quae ullam fuga numquam porro ab amet. Saepe atque ut quibusdam alias assumenda delectus cumque amet quis. Impedit nisi ab ad perferendis dicta quasi delectus perspiciatis asperiores ut tempore voluptatibus explicabo consectetur, iure esse! Nostrum, eveniet porro natus ab quas odio similique facilis est suscipit tempora vel. Alias, id incidunt!
             </p>
+            </div>
         </div>
     );
 }
